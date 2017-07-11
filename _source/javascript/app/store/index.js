9524e28a672c42520626c3784846538f16149859
Laís Tomaz <laisotomaz@gmail.com>
2017-07-11 18:50:25 -0300

    Add enterprise api call to the store

--- _source/javascript/app/store/index.js
+++ _source/javascript/app/store/index.js
@@ -7,6 +7,7 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
 	state: {
 		enterprises: [],
+		enterprise: {},
 	},
 	actions: {
 		LOAD_ENTERPRISES_LIST({ commit }) {
@@ -17,11 +18,24 @@ const store = new Vuex.Store({
 				console.log(err);
 			});
 		},
+		LOAD_ENTERPRISE({ commit }) {
+			axios.get('http://localhost:3000/enterprises/1').then((response) => {
+				commit('SET_ENTERPRISES_LIST', { res: response.data });
+				console.log(response.data);
+			}, (err) => {
+				// eslint-disable-next-line
+				console.log(err);
+			});
+		},
 	},
 	mutations: {
 		SET_ENTERPRISES_LIST(state, { list }) {
 			// eslint-disable-next-line
 			state.enterprises = list;
+		},
+		SET_ENTERPRISE(state, { res }) {
+			// eslint-disable-next-line
+			state.enterprise = res;
 		},
 	},
 	getters: {
