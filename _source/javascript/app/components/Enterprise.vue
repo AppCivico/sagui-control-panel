3b44f2a18361c0b3b2cdca07d1edd475e10993c8
Laís Tomaz <laisotomaz@gmail.com>
2017-07-11 18:49:07 -0300

    Create enterpise component

---
+++ _source/javascript/app/components/Enterprise.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="content-wrapper">
+		<!-- Content Header (Page header) -->
+		<section class="content-header">
+			<h1>
+			Empreendimento {{ enterprise.id }}
+		 	</h1>
+			<ol class="breadcrumb">
+				<li><router-link to="/"><i class="fa fa-dashboard"></i>Empreendimento</router-link></li>
+				<li class="active">{{ enterprise.name }}</li>
+			</ol>
+		</section>
+
+		<!-- Main content -->
+		<section class="content">
+		</section>
+		<!-- /.content -->
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Enterprise',
+	computed: {
+		enterprise() {
+			return this.$store.state.enterprise;
+		},
+	},
+	mounted() {
+		this.$store.dispatch('LOAD_ENTERPRISE');
+	},
+};
+</script>
