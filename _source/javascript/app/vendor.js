4b7613afbac4b154942f4a49b22a1b47cd67e01b
Laís Tomaz <lais@eokoe.com>
2017-07-12 18:49:43 -0300

    Create build vendor dist files

---
+++ _source/javascript/app/vendor.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+//Required files for AdminLTE theme
+//Import jQuery
+window.jQuery = require('../../../node_modules/jquery/dist/jquery');
+window.$ = window.jQuery;
+//Import Bootstrap
+const bootstrap = require('../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap');
+//Import AdminLTE js file
+const adminlte = require('../../../node_modules/admin-lte/dist/js/app');
