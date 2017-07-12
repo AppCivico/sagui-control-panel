efa384e90dfc07ec29389e029e13e7678c95f964
Laís Tomaz <lais@eokoe.com>
2017-07-12 16:57:13 -0300

    Remove mock router-link

--- _source/javascript/app/components/Hello.vue
+++ _source/javascript/app/components/Hello.vue
@@ -4,7 +4,6 @@
 		<h1>{{ msg }}</h1>
 		<h2>Essential Links</h2>
 		<ul>
-			<li> <router-link to="/oi">Go to Foo</router-link></li>
 			<li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
 			<li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
 			<li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
