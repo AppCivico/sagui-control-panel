f99b95950998387fa3f960fe89a1c75899aff698
Laís Tomaz <lais@eokoe.com>
2017-07-13 18:37:55 -0300

    Enhance ListEnterprises layout

--- _source/javascript/app/components/cListEnterprises.vue
+++ _source/javascript/app/components/cListEnterprises.vue
@@ -2,16 +2,31 @@
 	<div class="content-wrapper">
 		<!-- Content Header (Page header) -->
 		<section class="content-header">
-			<h1>{{ 'enterprises' | translate }}</h1>
-			<ol class="breadcrumb">
-				<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-				<li class="active">Empreendimentos</li>
-			</ol>
+			<h1>{{ 'choose_enterprise' | translate }}</h1>
 		</section>
 
 		<!-- Main content -->
 		<section class="content">
-			<div class="col-md-6" v-for="enterprise in enterprises" style="border: 1px solid #000;"><router-link :to="'/enterprises/'+enterprise.id">{{ enterprise.name }}</router-link></div>
+			<div class="row">
+				<div class="col-md-6" v-for="enterprise in enterprises">
+					<div class="info-box">
+						<span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+
+						<div class="info-box-content">
+							<span class="info-box-text">
+								<router-link :to="'/enterprises/'+enterprise.id">{{ enterprise.name }}</router-link>
+								<p>{{ enterprise.location.human_address }}</p>
+								<ul>
+									<li>{{ enterprise.data.complaints }} {{ 'complaints' | translate }}</li>
+									<li>{{ enterprise.data.cases }} {{ 'cases' | translate }}</li>
+									<li>{{ enterprise.data.actions }} {{ 'actions' | translate }}</li>
+									<li>{{ enterprise.data.surveys }} {{ 'surveys_completed' | translate }}</li>
+								</ul>
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 		<!-- /.content -->
 	</div>
