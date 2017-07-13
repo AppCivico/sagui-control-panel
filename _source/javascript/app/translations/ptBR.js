a753d63c79b20af52c6f6117001fa7c30e948c1b
Laís Tomaz <lais@eokoe.com>
2017-07-13 18:37:21 -0300

    Add new translations

--- _source/javascript/app/translations/ptBR.js
+++ _source/javascript/app/translations/ptBR.js
@@ -1,8 +1,14 @@
 /* eslint-disable */
 //I disabled it because it'll complain about the double quotes
 const ptBr = {
+	"choose_enterprise": "Escolha o empreendimento",
 	"enterprise": "Empreendimento",
 	"enterprises": "Empreendimentos",
+	"complaints": "apontamentos",
+	"cases": "ocorrências",
+	"actions": "contribuições / confirmações",
+	"surveys": "enquetes",
+	"surveys_completed": "enquetes respondidas"
 }
 /* eslint-enable */
 export { ptBr as default };
