bb999367b2ff1413e3e5f99d5bd6aa6c1137685a
Laís Tomaz <lais@eokoe.com>
2017-07-17 17:57:38 -0300

    Enhance New Survey component

--- _source/javascript/app/components/cNewSurvey.vue
+++ _source/javascript/app/components/cNewSurvey.vue
@@ -2,12 +2,58 @@
 	<div class="content-wrapper">
 		<!-- Content Header (Page header) -->
 		<section class="content-header">
-			<h1>{{ 'surveys' | translate }}</h1>
+			<h1>{{ 'surveys_new' | translate }}</h1>
 		</section>
 
 		<!-- Main content -->
 		<section class="content">
 			<div class="row">
+				<div class="col-md-6">
+					<div class="box box-solid">
+						<div class="box-header with-border">
+							<h3 class="box-title">Dados</h3>
+						</div>
+						<div class="box-body">
+							<div class="form-group">
+								<label>Text</label>
+								<input type="text" class="form-control" placeholder="Nome">
+			                </div>
+							<div class="form-group">
+								<label>Categoria</label>
+								<select class="form-control">
+									<option v-for="categorie in categories">{{ categorie }}</option>
+								</select>
+			                </div>
+			                <button type="button" class="btn btn-block btn-success">Cadastrar enquete</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="box box-solid">
+						<div class="box-header with-border">
+							<h3 class="box-title">Perguntas</h3>
+						</div>
+						<div class="box-body">
+							<div class="box box-warning" v-for="question in questions">
+								<div class="box-header with-border">
+									<h3 class="box-title">{{ question.title }}</h3>
+								</div>
+								<div class="box-body">
+									<div class="row">
+										<div class="col-md-12">
+											<h4>Opções de resposta</h4>
+										</div>
+
+										<div class="col-md-4" v-for="answer in question.answers">
+											<h5>{{ answer.title }}</h5>
+										</div>
+									</div>
+								</div>
+							</div>
+							<button type="button" class="btn btn-block btn-warning" data-toggle="modal" data-target="#modal-default">Adicionar pergunta</button>
+						</div>
+					</div>
+				</div>
 				<c-question></c-question>
 			</div>
 		</section>
@@ -23,5 +69,73 @@ export default{
 	components: {
 		cQuestion,
 	},
+	data() {
+		return {
+			oi: 'oi',
+			categories: ['Moradia', 'Alimentação', 'Locomoção'],
+			questions: [
+				{
+					type: 'farol',
+					title: 'Esta satisfeito com a sua moradia?',
+					answers: [
+						{
+							unit: 'red',
+							title: 'Não',
+						},
+						{
+							unit: 'yellow',
+							title: 'Médio',
+						},
+						{
+							unit: 'green',
+							title: 'Sim',
+						},
+					],
+				},
+				{
+					type: 'farol',
+					title: 'Esta satisfeito com a sua moradia?',
+					answers: [
+						{
+							unit: 'red',
+							title: 'Não',
+						},
+						{
+							unit: 'yellow',
+							title: 'Médio',
+						},
+						{
+							unit: 'green',
+							title: 'Sim',
+						},
+					],
+				},
+				{
+					type: 'farol',
+					title: 'Esta satisfeito com a sua moradia?',
+					answers: [
+						{
+							unit: 'red',
+							title: 'Não',
+						},
+						{
+							unit: 'yellow',
+							title: 'Médio',
+						},
+						{
+							unit: 'green',
+							title: 'Sim',
+						},
+					],
+				},
+			],
+		};
+	},
 };
 </script>
+
+<style scoped>
+	.content-header {
+		text-transform: capitalize;
+	}
+</style>
