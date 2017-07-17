21b8b43dbd4d355cdb1a066998b460fd7d4debdc
Laís Tomaz <lais@eokoe.com>
2017-07-17 16:25:44 -0300

    Create survey and question components

---
+++ _source/javascript/app/components/cNewSurvey.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="content-wrapper">
+		<!-- Content Header (Page header) -->
+		<section class="content-header">
+			<h1>{{ 'surveys' | translate }}</h1>
+		</section>
+
+		<!-- Main content -->
+		<section class="content">
+			<div class="row">
+				<c-question></c-question>
+			</div>
+		</section>
+		<!-- /.content -->
+	</div>
+</template>
+
+<script>
+import cQuestion from './cQuestion.vue';
+
+export default{
+	name: 'cNewSurvey',
+	components: {
+		cQuestion,
+	},
+};
+</script>
