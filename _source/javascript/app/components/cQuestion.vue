480224282d174e84fdbfa78d254ad7c3f22450e4
Laís Tomaz <lais@eokoe.com>
2017-07-19 13:47:58 -0300

    Add methods to Question component

--- _source/javascript/app/components/cQuestion.vue
+++ _source/javascript/app/components/cQuestion.vue
@@ -1,24 +1,79 @@
 <script>
 export default {
 	name: 'cQuestion',
+	data() {
+		return {
+			multiple: 1,
+			type: '',
+			types: ['farol', 'multiple', 'textarea'],
+		};
+	},
+	methods: {
+		addOption(event) {
+			const target = event.target;
+			const option = `<div class="form-group">
+								<label>Opção ${this.multiple + 1}</label>
+								<input type="text" class="form-control" placeholder="Digite aqui uma opção">
+							</div>`;
+			target.insertAdjacentHTML('beforebegin', option);
+			this.multiple += 1;
+		},
+		cleanFields() {
+			const title = document.querySelector('#new-question input[name="title"]');
+			title.value = '';
+			this.type = '';
+		},
+	},
 };
 </script>
 
 <template>
-	<div class="modal fade in" id="modal-default">
+	<div class="modal fade in" id="new-question">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Cancelar" @click="cleanFields()">
 						<span aria-hidden="true">×</span></button>
-					<h4 class="modal-title">Default Modal</h4>
+					<h4 class="modal-title">Nova pergunta</h4>
 				</div>
 				<div class="modal-body">
-					<p>One fine body…</p>
+					<div class="form-group">
+						<label>Título</label>
+						<input type="text" class="form-control" name="title" placeholder="Título">
+					</div>
+					<div class="form-group">
+						<label>Tipo</label>
+						<select class="form-control" v-model="type">
+							<option value="">Escolha um tipo de resposta</option>
+							<option v-for="option in types" :value="option">{{ option }}</option>
+						</select>
+					</div>
+					<hr>
+					<div v-if="type == 'farol'">
+						<div class="form-group">
+							<label>Verde</label>
+							<input type="text" class="form-control" placeholder="Verde">
+						</div>
+						<div class="form-group">
+							<label>Amarelo</label>
+							<input type="text" class="form-control" placeholder="Amarelo">
+						</div>
+						<div class="form-group">
+							<label>Vermelho</label>
+							<input type="text" class="form-control" placeholder="Vermelho">
+						</div>
+					</div>
+					<div v-if="type == 'multiple'">
+						<div class="form-group">
+							<label>Opção 1</label>
+							<input type="text" class="form-control" placeholder="Digite aqui uma opção">
+						</div>
+						<button type="button" class="btn btn-primary" @click="addOption($event)">Adicionar opção</button>
+					</div>
 				</div>
 				<div class="modal-footer">
-					<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Save changes</button>
+					<button type="button" class="btn btn-default pull-left" data-dismiss="modal" @click="cleanFields()">Cancelar</button>
+					<button type="button" class="btn btn-primary">Adicionar</button>
 				</div>
 			</div>
 			<!-- /.modal-content -->
