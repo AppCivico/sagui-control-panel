0aff191167fb356150eb4cbba4ce5e5e4921efab
Laís Tomaz <lais@eokoe.com>
2017-07-19 17:58:23 -0300

    Remove filter file

--- _source/javascript/app/filters/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-// register
-Vue.filter('capitalize', function (value) { // eslint-disable-line prefer-arrow-callback, func-names
-	if (!value) return '';
-	return value.toString().charAt(0).toUpperCase() + value.toString().slice(1);
-});
