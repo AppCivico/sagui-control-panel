048c1d7e15876b1108fbee28d0c9dcbb190171f9
Laís Tomaz <lais@eokoe.com>
2017-07-19 17:58:42 -0300

    Add capitalize filter

--- _source/javascript/app/application.js
+++ _source/javascript/app/application.js
@@ -8,6 +8,11 @@ import store from './store';
 import router from './router';
 import translations from './translations';
 
+Vue.filter('capitalize', function (value) { // eslint-disable-line prefer-arrow-callback, func-names
+	if (!value) return '';
+	return value.toString().charAt(0).toUpperCase() + value.toString().slice(1);
+});
+
 new Vue({ // eslint-disable-line no-new
 	el: '#app',
 	store,
