2e2997811e6913d1a4615d0dc4207ef5390b61b8
Laís Tomaz <lais@eokoe.com>
2017-08-01 10:25:21 -0300

    Translate menu

--- _source/javascript/app/components/cMenu.vue
+++ _source/javascript/app/components/cMenu.vue
@@ -10,26 +10,18 @@ export default{
 	<section class="sidebar">
 		<!-- sidebar menu: : style can be found in sidebar.less -->
 		<ul class="sidebar-menu">
-			<li class="header">MENU</li>
-			<li>
-				<a href="pages/widgets.html">
-					<i class="fa fa-th"></i> <span>Widgets</span>
-					<span class="pull-right-container">
-						<small class="label pull-right bg-green">new</small>
-					</span>
-				</a>
-			</li>
 			<li class="treeview">
 				<a href="#">
 					<i class="fa fa-pie-chart"></i>
-					<span>{{ 'surveys' | translate }}</span>
+					<span>{{ 'surveys' | translate | capitalize }}</span>
 					<span class="pull-right-container">
 						<i class="fa fa-angle-left pull-right"></i>
 					</span>
 				</a>
 				<ul class="treeview-menu">
-					<li><router-link to="/categories"><i class="fa fa-circle-o"></i> Enquetes</router-link></li>
-					<li><router-link to="/new-survey"><i class="fa fa-circle-o"></i> Nova enquete</router-link></li>
+					<li><router-link to="/categories"><i class="fa fa-circle-o"></i> {{ 'categories' | translate | capitalize }}</router-link></li>
+					<li><router-link to="/new-survey"><i class="fa fa-circle-o"></i> {{ 'new' | translate | capitalize }} {{ 'survey' | translate | capitalize }}</router-link></li>
+					<li><router-link to=""><i class="fa fa-circle-o"></i> {{ 'results' | translate | capitalize }}</router-link></li>
 				</ul>
 			</li>
 		</ul>
