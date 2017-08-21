ec2701d622f7e6f823b4db789e942da012dfe3bf
Laís Tomaz <lais@eokoe.com>
2017-08-21 11:51:04 -0300

    Fix test for ListEnterprises component

--- _source/javascript/spec/unit/cListEnterprises.spec.js
+++ _source/javascript/spec/unit/cListEnterprises.spec.js
@@ -4,23 +4,34 @@ import Vue from 'vue';
 import { chai, expect } from 'chai';
 import store from '../../app/store';
 import translations from '../../app/translations';
+import router from '../../app/router';
 import cListEnterprises from '../../app/components/cListEnterprises.vue';
 
 describe('cListEnterprises.vue', () => {
 	it('should translate content', () => {
+		Vue.filter('capitalize', function (value) { // eslint-disable-line prefer-arrow-callback, func-names
+			if (!value) return '';
+			return value.toString().charAt(0).toUpperCase() + value.toString().slice(1);
+		});
 		const vm = new Vue({
 			el: document.createElement('div'),
 			store,
 			translations,
+			router,
 			render: h => h(cListEnterprises),
 		});
 		expect(vm.$el.querySelector('h1').textContent).to.equal('Escolha o empreendimento');
 	});
 	it('should load store', () => {
+		Vue.filter('capitalize', function (value) { // eslint-disable-line prefer-arrow-callback, func-names
+			if (!value) return '';
+			return value.toString().charAt(0).toUpperCase() + value.toString().slice(1);
+		});
 		const vm = new Vue({
 			el: document.createElement('div'),
 			store,
 			translations,
+			router,
 			render: h => h(cListEnterprises),
 		});
 		expect(vm.$store.state.enterprises).to.be.an('array');
