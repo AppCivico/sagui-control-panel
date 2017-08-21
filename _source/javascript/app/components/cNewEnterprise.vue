e307346fd5dfec9ecddda0950c36561f041715c2
Laís Tomaz <lais@eokoe.com>
2017-08-21 11:37:48 -0300

    Create NewEnterprise component

---
+++ _source/javascript/app/components/cNewEnterprise.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+	name: 'cNewEnterprise',
+	methods: {
+	},
+};
+</script>
+
+<template>
+	<div>
+		<!-- Content Header (Page header) -->
+		<section class="content-header">
+			<h1>{{ 'new' | translate | capitalize }} {{ 'enterprise' | translate | capitalize }}</h1>
+		</section>
+
+		<!-- Main content -->
+		<section class="content">
+			<div class="box box-solid">
+				<div class="box-body no-padding">
+
+				</div>
+			</div>
+		</section>
+		<!-- /.content -->
+	</div>
+</template>
