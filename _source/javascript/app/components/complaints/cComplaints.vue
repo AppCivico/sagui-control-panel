cf6fb7cb54d72e59c9beefa179541ad7ea024d8d
Laís Tomaz <lais@eokoe.com>
2017-08-29 16:24:05 -0300

    Add method to calculate remaining actions

--- _source/javascript/app/components/complaints/cComplaints.vue
+++ _source/javascript/app/components/complaints/cComplaints.vue
@@ -9,17 +9,32 @@ export default {
 		selectedEnterprise() {
 			return this.$store.state.selectedEnterprise;
 		},
+		categories() {
+			return this.$store.state.categories;
+		},
 	},
 	watch: {
 		status() {
-			console.log('mudou');
 			this.$store.dispatch('LOAD_COMPLAINTS_LIST', { enterprise: this.selectedEnterprise, status: this.status });
 		},
 	},
 	mounted() {
+		this.$store.dispatch('LOAD_CATEGORIES_LIST', this.selectedEnterprise);
 		this.$store.dispatch('LOAD_COMPLAINTS_LIST', { enterprise: this.selectedEnterprise, status: this.status });
 	},
 	methods: {
+		remainingActions(actions) {
+			if (actions < 30) {
+				return 30 - actions;
+			}
+			return false;
+		},
+		categoryName(id) {
+			const category = this.categories.filter((cat) => {
+				return cat.id === id;
+			});
+			return category[0].title;
+		},
 	},
 };
 </script>
@@ -36,12 +51,14 @@ export default {
 		<section class="content">
 			<div class="box box-solid">
 				<div class="box-body">
-					<template v-for="complaint in complaints">
-						<div>
-							<h4><router-link to="/">{{ complaint.title }}</router-link></h4>
+					<div class="row">
+						<div class="col-md-6" v-for="complaint in complaints">
+							<h4><router-link to="/">{{ complaint.title }}</router-link> <span v-if="remainingActions(complaint.actions)">Faltam: {{ remainingActions(complaint.actions) }} confirmações</span></h4>
+							<span>{{ complaint.address }}</span><br>
+							<span><strong>Categoria:</strong> {{ categoryName(complaint.category) }}</span>
 							<p>{{ complaint.description }}</p>
 						</div>
-					</template>
+					</div>
 				</div>
 			</div>
 		</section>
