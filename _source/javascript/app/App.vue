fb2c31b89e1e520a9d02eb45ec01b39b88dd4510
Laís Tomaz <lais@eokoe.com>
2017-09-06 17:23:20 -0300

    Move session methods to App.vue

--- _source/javascript/app/App.vue
+++ _source/javascript/app/App.vue
@@ -11,6 +11,29 @@ export default {
 		cMenu,
 		cAlert,
 	},
+	computed: {
+		user() {
+			return this.$store.state.user;
+		},
+	},
+	beforeCreate() {
+		// check if api-key exists, if negative, send user to login page
+		const apiKey = sessionStorage.getItem('api-key');
+		if (!apiKey) {
+			this.$router.push('/');
+		}
+	},
+	mounted() {
+		// check if api-key exists, if yes, load user data
+		if (Object.keys(this.user).length === 0 && sessionStorage.getItem('api-key')) {
+			this.getUser(sessionStorage.getItem('api-key'));
+		}
+	},
+	methods: {
+		getUser(apiKey) {
+			this.$store.dispatch('LOAD_USER', apiKey);
+		},
+	},
 };
 </script>
 
