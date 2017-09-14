2d59fe0a150d89bf6d139e938bf3c8a9fecffc9d
Laís Tomaz <lais@eokoe.com>
2017-09-14 19:36:06 -0300

    Update iconPicker

--- _source/javascript/app/components/utilities/cIconPicker.vue
+++ _source/javascript/app/components/utilities/cIconPicker.vue
@@ -19,6 +19,11 @@ export default {
 				const id = data.split('\n')
 					.filter(item => item.indexOf(' id:') > 0);
 				const array = id.map(item => item.replace('id:', '').trim());
+				const iconsDiv = document.querySelector('.iconPicker__icons');
+
+				array.map((item) => {
+					iconsDiv.insertAdjacentHTML('beforeend', `<a href="#" @click="this.selectIcon($event)"><i class="fa fa-${item}"></i></a>`);
+				});
 				console.log(array);
 			}, (err) => {
 				console.error(err);
@@ -29,10 +34,37 @@ export default {
 				this.getIconsList();
 			}
 		},
+		selectIcon(event) {
+			console.log(event.target);
+		},
 	},
 };
 </script>
 
 <template>
-	<h1 v-observe-visibility="visibilityChanged">Icons</h1>
+	<div v-observe-visibility="visibilityChanged" id="iconPicker">
+		<div class="iconPicker__icons"></div>
+	</div>
 </template>
+
+<style scoped>
+	#iconPicker {
+		position: relative;
+	}
+	.iconPicker__icons {
+		position: absolute;
+		top: 0;
+		left: 0;
+		max-width: 320px;
+	}
+	.iconPicker__icons a {
+		display: inline-block;
+		width: 25%;
+		border: 1px solid #000;
+		text-align: center;
+		vertical-align: middle;
+	}
+	.iconPicker__icons a i {
+		vertical-align: middle;
+	}
+</style>
