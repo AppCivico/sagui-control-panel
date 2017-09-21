eef49716a02e0f7e91a9c6dc69d171245218840c
Laís Tomaz <lais@eokoe.com>
2017-09-21 12:23:27 -0300

    Fix name issue on list enterprises component

--- _source/javascript/app/components/enterprises/cListEnterprises.vue
+++ _source/javascript/app/components/enterprises/cListEnterprises.vue
@@ -15,7 +15,6 @@ export default {
 		this.$store.dispatch('LOAD_ENTERPRISES_LIST').then(() => {
 			this.getThumbnails();
 		});
-		// jQuery.AdminLTE.layout.activate();
 	},
 	methods: {
 		getThumbnails() {
@@ -28,60 +27,6 @@ export default {
 				}
 			});
 		},
-		/* fix() {
-			// Remove overflow from .wrapper if layout-boxed exists
-			$('.layout-boxed > .wrapper').css('overflow', 'hidden');
-			// Get window height and the wrapper height
-			const footerHeight = $('.main-footer').outerHeight() || 0;
-			const neg = $('.main-header').outerHeight() + footerHeight;
-			const windowHeight = $(window).height();
-			const sidebarHeight = $('.sidebar').height() || 0;
-			// Set the min-height of the content and sidebar based on the
-			// the height of the document.
-			if ($('body').hasClass('fixed')) {
-				$('.content-wrapper, .right-side').css('min-height', windowHeight - footerHeight);
-			} else {
-				let postSetWidth;
-				if (windowHeight >= sidebarHeight) {
-				$('.content-wrapper, .right-side').css('min-height', windowHeight - neg);
-				postSetWidth = windowHeight - neg;
-			} else {
-				$('.content-wrapper, .right-side').css('min-height', sidebarHeight);
-				postSetWidth = sidebarHeight;
-			}
-
-			// Fix for the control sidebar height
-			const controlSidebar = $($.AdminLTE.options.controlSidebarOptions.selector);
-			if (typeof controlSidebar !== 'undefined') {
-				if (controlSidebar.height() > postSetWidth)
-					$('.content-wrapper, .right-side').css('min-height', controlSidebar.height());
-				}
-			}
-		},
-		fixSidebar() {
-			// Make sure the body tag has the .fixed class
-			if (!$('body').hasClass('fixed')) {
-				if (typeof $.fn.slimScroll !== 'undefined') {
-					$('.sidebar').slimScroll({ destroy: true }).height('auto');
-				}
-				return;
-			} else if (typeof $.fn.slimScroll === 'undefined' && window.console) {
-				window.console.error('Error: the fixed layout requires the slimscroll plugin!');
-			}
-			// Enable slimscroll for fixed layout
-			if ($.AdminLTE.options.sidebarSlimScroll) {
-				if (typeof $.fn.slimScroll !== 'undefined') {
-					// Destroy if it exists
-					$('.sidebar').slimScroll({ destroy: true }).height('auto');
-					// Add slimscroll
-					$('.sidebar').slimScroll({
-						height: `${($(window).height() - $('.main-header').height())}px`,
-						color: 'rgba(0,0,0,0.2)',
-						size: '3px',
-					});
-				}
-			}
-		}, */
 	},
 };
 </script>
@@ -103,7 +48,7 @@ export default {
 
 						<div class='col-md-9 info-box-content'>
 							<span class='info-box-text'>
-								<router-link :to=''/enterprises/'+enterprise.id'>{{ enterprise.name }}</router-link>
+								<router-link :to="'/enterprises/'+enterprise.id">{{ enterprise.name }}</router-link>
 								<p>{{ enterprise.human_address }}</p>
 								<ul class='list-unstyled'>
 									<li>{{ enterprise.data.complaints }} {{ 'complaints' | translate }}</li>
