50b36b95752c0b6714d64544da1d45cae95cea70
Laís Tomaz <lais@eokoe.com>
2017-09-21 19:08:53 -0300

    Add iconPicker to categorie component

--- _source/javascript/app/components/surveys/cCategorie.vue
+++ _source/javascript/app/components/surveys/cCategorie.vue
@@ -1,12 +1,21 @@
 <script>
 import Vue from 'vue';
 import methods from '../../methods';
+import cIconPicker from '../utilities/iconPicker/index.vue';
 
 export default {
 	name: 'cCategorie',
 	props: {
 		category: Object,
 		isEditing: Boolean,
+	},
+	data() {
+		return {
+			selectedIcon: '',
+		};
+	},
+	components: {
+		cIconPicker,
 	},
 	computed: {
 		selectedEnterprise() {
@@ -44,20 +53,39 @@ export default {
 				methods.addError(title.parentNode, Vue.i18n.translate('required-field'));
 				valid = false;
 			}
+			if (this.selectedIcon === '') {
+				this.$store.dispatch('CHANGE_ALERT_MESSAGE', Vue.i18n.translate('no-icon'));
+				valid = false;
+			}
 			if (valid) {
 				if (this.isEditing) {
 					const category = {
 						name: title.value,
 						id: this.category.id,
+						icon: this.selectedIcon,
 					};
 					this.$store.dispatch('EDIT_CATEGORY', category);
 				} else {
 					const category = {
 						name: title.value,
 						dimension: 'category',
+						icon: this.selectedIcon,
 					};
 					this.$store.dispatch('ADD_CATEGORY', category);
 				}
+			}
+		},
+		selectIcon(value) {
+			const icon = document.querySelector('#new-category .showIcon');
+			icon.classList.add(value.className);
+			this.selectedIcon = value.cssValue;
+		},
+		showIconPicker() {
+			const el = document.querySelector('#new-category #iconPicker');
+			if (el.ownerDocument.defaultView.getComputedStyle(el, null).display === 'none') {
+				el.style.display = 'block';
+			} else {
+				el.style.display = 'none';
 			}
 		},
 	},
@@ -80,6 +108,16 @@ export default {
 						<label>{{ 'title' | translate  | capitalize }}</label>
 						<input v-if="this.isEditing" type="text" class="form-control" name="title" placeholder="Título" :value="this.category.name">
 						<input v-else type="text" class="form-control" name="title" placeholder="Título">
+						<hr>
+						<label>{{ 'icon' | translate  | capitalize }}</label>
+						<div>
+							<i :class="'fa showIcon '+this.category.icon" v-if="this.isEditing"></i>
+							<i class="fa showIcon" v-else></i>
+						</div>
+
+						<button type="button" class="btn btn-default" @click="showIconPicker()" v-if="this.isEditing">{{ 'change' | translate  | capitalize }} {{ 'icon' | translate }}</button>
+						<button type="button" class="btn btn-default" @click="showIconPicker()" v-else>{{ 'select' | translate  | capitalize }} {{ 'icon' | translate }}</button>
+						<c-icon-picker :seachbox="'search' | translate  | capitalize" v-on:selectIcon="selectIcon"></c-icon-picker>
 					</div>
 				</div>
 				<div class="modal-footer">
@@ -93,3 +131,20 @@ export default {
 		<!-- /.modal-dialog -->
 	</div>
 </template>
+
+<style scope>
+	#iconPicker {
+		display: none;
+	}
+	.showIcon {
+	    width: 40px;
+	    height: 40px;
+	    padding: 12px;
+	    margin: 0 12px 12px 0;
+	    text-align: center;
+	    border-radius: 3px;
+	    font-size: 14px;
+	    box-shadow: 0 0 0 1px #ddd;
+	    color: inherit;
+	}
+</style>
