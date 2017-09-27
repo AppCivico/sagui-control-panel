b63cdab6ae677a6adfe81e045f4fddaa6cbc85cf
Laís Tomaz <lais@eokoe.com>
2017-09-27 18:49:22 -0300

    Update config file

--- _source/javascript/app/config.js
+++ _source/javascript/app/config.js
@@ -2,6 +2,7 @@ const config = {
 	url: 'http://dev-sagui-api.eokoe.com',
 	name: 'Projeto Sagui',
 	devapi: 'http://dev-sagui-api.eokoe.com/v1',
+	api: 'https://fakeapi.eokoe.com',
 };
 
 export { config as default };
