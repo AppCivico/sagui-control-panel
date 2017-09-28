64763474a03e5710d38113d22ac174aa40733645
Laís Tomaz <lais@eokoe.com>
2017-09-28 15:54:26 -0300

    Enhance complaint component

--- _source/javascript/app/components/complaints/cComplaint.vue
+++ _source/javascript/app/components/complaints/cComplaint.vue
@@ -1,10 +1,25 @@
 <script>
+import cAnswer from './cAnswer.vue';
+
 export default {
-	name: 'cComplaints',
+	name: 'cComplaint',
 	props: ['id'],
+	components: {
+		cAnswer,
+	},
+	data() {
+		return {
+			isEditing: false,
+			editingAnswer: {},
+			editingAnswerIndex: 0,
+		};
+	},
 	computed: {
 		complaint() {
 			return this.$store.state.complaints.complaint;
+		},
+		comments() {
+			return this.$store.state.complaints.complaint.comments;
 		},
 	},
 	mounted() {
@@ -16,6 +31,25 @@ export default {
 				return 30 - actions;
 			}
 			return false;
+		},
+		newAnswer() {
+			this.isEditing = false;
+		},
+		addAnswer(result) {
+			this.comments.push(result.newAnswer.data);
+			this.comments[this.comments.length - 1].id = result.id;
+		},
+		converteDate(timestamp) {
+			return timestamp
+				.split('T')[0]
+				.split('-')
+				.reverse()
+				.join('/');
+		},
+		setEditingAnswer(answer, index) {
+			this.editingAnswer = answer;
+			this.editingAnswerIndex = index;
+			this.isEditing = true;
 		},
 	},
 };
@@ -29,32 +63,34 @@ export default {
 				<div class="col-md-12">
 					<div class="box box-solid complaint">
 						<div class="box-header with-border">
-							<h3 class="box-title"><router-link to="/">{{ complaint.title }}</router-link> <span v-if="remainingActions(complaint.confirmations)" class="remaining">Faltam: {{ remainingActions(complaint.confirmations) }} confirmações</span></h3>
+							<!-- adicionar v-if quando tiver flag de status -->
+							<h3 class="box-title"><router-link to="/">{{ complaint.title | capitalize }}</router-link> <span v-if="remainingActions(complaint.confirmations)" class="remaining">Faltam: {{ remainingActions(complaint.confirmations) }} confirmações</span></h3>
 						</div>
 						<div class="box-body">
 							<span>{{ complaint.human_address }}</span><br>
 							<span>{{ complaint.axis.name }}</span><br><br>
 							<p>{{ complaint.description }}</p>
-
-							<h4>Respostas:</h4>
-							<template v-if="complaint.comments.length > 1">
-								<div v-for="comment in comments" >
-									<span>{{ comment.created_at }}</span>
-									<p>{{ comment.content }}</p>
+						</div>
+						<div class="box-footer box-comments">
+							<h5><strong>Respostas:</strong></h5>
+							<div class="box-comment" v-for="(answer, index) in comments">
+								<div class="comment-text" >
+									<span class="username" v-if="answer.created_at">
+										{{ converteDate(answer.created_at) }}
+										<!-- <button type="button" aria-label="Editar" data-toggle="modal" data-target="#answer" class="edit-button" @click="setEditingAnswer(answer, index)"><i class="fa fa-edit"></i></button> -->
+									</span><!-- /.username -->
+									{{ answer.content }}
 								</div>
-							</template>
-							<template v-else>
-								<div class="alert alert-info">
-									{{ 'no-answers' | translate }}
-								</div>
-							</template>
-							<button type="button" class="btn btn-info">{{ 'new' | translate | capitalize }} {{ 'answer' | translate }}</button>
+								<!-- /.comment-text -->
+							</div>
+							<button type="button" class="btn btn-info" data-toggle="modal" data-target="#answer" @click="newAnswer()">{{ 'new' | translate | capitalize }} {{ 'answer' | translate }}</button>
 						</div>
 					</div>
 				</div>
 			</div>
 		</section>
 		<!-- /.content -->
+		<c-answer :answer="this.editingAnswer" :complaintId="this.id" :isEditing="this.isEditing" v-on:newAnswer="addAnswer"></c-answer>
 	</div>
 </template>
 
@@ -72,5 +108,8 @@ export default {
 	}
 	.complaint .box-body span {
 		font-size: 0.9em;
+	}
+	.box-comments .comment-text {
+		margin-left: 0;
 	}
 </style>
--- _source/javascript/app/store/modules/complaints.js
+++ _source/javascript/app/store/modules/complaints.js
@@ -30,16 +30,42 @@ axios.interceptors.response.use((response) => {
 
 const state = {
 	complaints: [],
+	complaint: {},
 };
 
 // actions
 const actions = {
-	LOAD_COMPLAINTS_LIST({ commit }, options) { // eslint-disable-line no-unused-vars
-		axios.get(`${config.api}/complaints?status=${options.status}&enterprise=${options.enterprise}`).then((response) => {
+	LOAD_COMPLAINTS_LIST({ commit, rootState }, options) { // eslint-disable-line no-unused-vars
+		axios.get(`${config.devapi}/enterprises/${rootState.selectedEnterprise}/complaints?api_key=${rootState.auth.apiKey}`).then((response) => {
 			commit('SET_COMPLAINTS_LIST', { list: response.data });
 		}, (err) => {
 			console.error(err);
 			commit('SET_ALERT_MESSAGE', { res: { message: Vue.i18n.translate('request-error') } }, { root: true });
+		});
+	},
+	LOAD_COMPLAINT({ commit, rootState }, id) { // eslint-disable-line no-unused-vars
+		axios.get(`${config.devapi}/complaints/${id}?api_key=${rootState.auth.apiKey}`).then((response) => {
+			commit('SET_COMPLAINT', { res: response.data });
+		}, (err) => {
+			console.error(err);
+			commit('SET_ALERT_MESSAGE', { res: { message: Vue.i18n.translate('request-error') } }, { root: true });
+		});
+	},
+	SAVE_ANSWER({ commit, rootState }, info) { // eslint-disable-line no-unused-vars
+		return new Promise((resolve) => {
+			axios({
+				method: 'POST',
+				url: `${config.devapi}/complaints/${info.id}/comments?api_key=${rootState.auth.apiKey}`,
+				data: info.data,
+				headers: { 'Content-Type': 'application/json' },
+			})
+			.then((response) => {
+				commit('SET_ALERT_MESSAGE', { res: { message: Vue.i18n.translate('new-answer') } }, { root: true });
+				resolve(response);
+			}, (err) => {
+				console.error(err);
+				commit('SET_ALERT_MESSAGE', { res: { message: Vue.i18n.translate('request-error') } }, { root: true });
+			});
 		});
 	},
 };
@@ -48,6 +74,9 @@ const actions = {
 const mutations = {
 	SET_COMPLAINTS_LIST(state, { list }) {
 		state.complaints = list;
+	},
+	SET_COMPLAINT(state, { res }) {
+		state.complaint = res;
 	},
 };
 
