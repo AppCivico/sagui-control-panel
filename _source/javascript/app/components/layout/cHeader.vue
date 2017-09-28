1cb4e62a3e40994741b588b9e38d37c0aaf29207
Laís Tomaz <lais@eokoe.com>
2017-09-28 17:09:15 -0300

    Update default user profile image

--- _source/javascript/app/components/layout/cHeader.vue
+++ _source/javascript/app/components/layout/cHeader.vue
@@ -96,7 +96,7 @@ export default {
 					<!-- User Account: style can be found in dropdown.less -->
 					<li class="dropdown user user-menu">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+							<img src="dist/img/user-default.png" class="user-image" alt="User Image">
 							<span class="hidden-xs">{{ user.name }}</span>
 						</a>
 						<ul class="dropdown-menu">
