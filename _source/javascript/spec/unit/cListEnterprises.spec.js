73f7233b91ae255cba9aed308a5f588aaed7e13b
Laís Tomaz <lais@eokoe.com>
2017-10-02 18:22:01 -0300

    Update list enterprise tests

--- _source/javascript/spec/unit/cListEnterprises.spec.js
+++ _source/javascript/spec/unit/cListEnterprises.spec.js
@@ -7,21 +7,28 @@ import translations from '../../app/translations';
 import router from '../../app/router';
 import cListEnterprises from '../../app/components/enterprises/cListEnterprises.vue';
 
+
+Vue.filter('capitalize', function (value) { // eslint-disable-line prefer-arrow-callback, func-names
+	if (!value) return '';
+	return value.toString().charAt(0).toUpperCase() + value.toString().slice(1);
+});
+const vm = new Vue({
+	el: document.createElement('div'),
+	store,
+	router,
+	translations,
+	render: h => h(cListEnterprises),
+});
+
+
 describe('cListEnterprises.vue', () => {
-	it('should load store', () => {
-		Vue.filter('capitalize', function (value) { // eslint-disable-line prefer-arrow-callback, func-names
-			if (!value) return '';
-			return value.toString().charAt(0).toUpperCase() + value.toString().slice(1);
-		});
-		const vm = new Vue({
-			el: document.createElement('div'),
-			store,
-			translations,
-			router,
-			render: h => h(cListEnterprises),
-		});
+	it('should get auth', () => {
+		expect(vm.$store.state.auth.authorization).to.be.an('boolean');
+	});
+	it('load enterprise list', () => {
 		expect(vm.$store.state.enterprises.enterprises).to.be.an('array');
 	});
-
-	// Create test for the ajax call when real api is working
+	it('will translate content', () => {
+		expect(vm.$el.querySelector('h1').textContent).to.equal(Vue.i18n.translate('choose_enterprise'));
+	});
 });
