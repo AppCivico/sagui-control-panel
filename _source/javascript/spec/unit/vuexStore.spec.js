d055cbc6bee8fc08ec4bf1b28349cff2aaa8ffe3
Laís Tomaz <lais@eokoe.com>
2017-10-02 18:50:34 -0300

    Add vuex store test

---
+++ _source/javascript/spec/unit/vuexStore.spec.js
@@ -0,0 +1,26 @@
+// Always use the following line so ESlintwon't complain about Chai import
+/* eslint-disable no-unused-vars, arrow-body-style, no-param-reassign, no-console */
+import Vue from 'vue';
+import { chai, expect } from 'chai';
+import axios from 'axios';
+
+import config from '../../app/config';
+
+describe('vuexStore.vue', () => {
+	it('should login', () => {
+		axios({
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			url: `${config.devapi}/auth/signin`,
+			data: {
+				email: 'daniel@eokoe.com',
+				password: 'test',
+			},
+		})
+		.then((response) => {
+			expect(response.data).to.be.an('object');
+		}, (err) => {
+			expect(err.data).to.be.an('object');
+		});
+	});
+});
