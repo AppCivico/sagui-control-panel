0369f6b28bd72cfe16bca4d6db29a73e0cf383b5
Laís Tomaz <lais@eokoe.com>
2017-10-18 15:08:50 -0200

    Create footer component

---
+++ _source/javascript/app/components/layout/cFooter.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+	name: 'cFooter',
+};
+</script>
+
+<template>
+	<footer class="main-footer">
+		<div>
+			<strong>{{ 'developed-by' | translate | capitalize }} <a href="https://appcivico.com" target="_blank">AppCívico</a> {{ 'and' | translate }} <a href="http://portal.fgv.br/" target="_blank">FVG</a>.</strong>
+		</div>
+	</footer>
+</template>
